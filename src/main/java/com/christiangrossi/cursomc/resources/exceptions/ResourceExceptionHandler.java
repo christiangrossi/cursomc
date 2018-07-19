fb6a594e7a8d5a90fb6eedb99b602becd44e47db
christiangrossi <christian.grossi@outlook.com>
2018-07-19 17:01:00 -0300

    estados e cidades

--- src/main/java/com/christiangrossi/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/christiangrossi/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.christiangrossi.cursomc.resources.exceptions;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
