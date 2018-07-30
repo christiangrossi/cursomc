323e97cec1ad64289c59688951c2bf831f4251a3
christiangrossi <christian.grossi@outlook.com>
2018-07-30 00:40:02 -0300

    Validação customizada, email não podendo ser duplicado no banco

--- src/main/java/com/christiangrossi/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/christiangrossi/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.christiangrossi.cursomc.domain.Cliente;
 import com.christiangrossi.cursomc.domain.enums.TipoCliente;
 import com.christiangrossi.cursomc.dto.ClienteNewDTO;
+import com.christiangrossi.cursomc.repositories.ClienteRepository;
 import com.christiangrossi.cursomc.resources.exceptions.FieldMessage;
 import com.christiangrossi.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	private ClienteRepository repository;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -19,14 +26,20 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
-		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
-		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPNPJ inválido"));
 		}
+
+		Cliente aux = repository.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Esse endereço de e-mail já está sendo utilizado"));
+		}
+
 		// inclua os testes aqui, inserindo erros na lista
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
